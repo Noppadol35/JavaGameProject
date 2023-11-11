65709ed20dc03ae698c4ac457ae64eef126c9533
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2023-11-12 04:03:57 +0700

    new commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,38 @@
+package main;
+
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+public class GameWindow {
+    public final JFrame jframe;
+
+    public GameWindow(GamePanel gamePanel) {
+
+        jframe = new JFrame();
+        jframe.setTitle("Treasure Hunters");
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+
+        jframe.setResizable(false);
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+        jframe.addWindowFocusListener(new WindowFocusListener() {
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                gamePanel.getGame().windowFocusLost();
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+    }
+
+}
